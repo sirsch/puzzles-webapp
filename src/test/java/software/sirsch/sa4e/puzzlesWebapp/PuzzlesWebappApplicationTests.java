5fecaae393343b9ea0d939f0bcb3e7cbd6af3165
Sebastian Irsch <com@seb.in2u.eu>
2023-03-03 17:34:00 +0100

    Projekt mittels start.spring.io erstellt

---
+++ src/test/java/software/sirsch/sa4e/puzzlesWebapp/PuzzlesWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package software.sirsch.sa4e.puzzlesWebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuzzlesWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
