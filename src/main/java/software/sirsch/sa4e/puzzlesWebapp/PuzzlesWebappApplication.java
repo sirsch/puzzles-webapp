384990bc8195fba777bca6405405db1edb6e54b1
Sebastian Irsch <com@seb.in2u.eu>
2023-03-04 11:18:52 +0100

    Projekt konfiguriert

--- src/main/java/software/sirsch/sa4e/puzzlesWebapp/PuzzlesWebappApplication.java
+++ src/main/java/software/sirsch/sa4e/puzzlesWebapp/PuzzlesWebappApplication.java
@@ -1,13 +1,31 @@
 package software.sirsch.sa4e.puzzlesWebapp;
 
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Diese Klasse stellt die Spring-Boot-Anwendung bereit.
+ *
+ * @author sirsch
+ */
 @SpringBootApplication
 public class PuzzlesWebappApplication {
 
-	public static void main(String[] args) {
+	/**
+	 * Dieses Feld zeigt an, dass es sich nicht um eine Hilfsklasse handelt.
+	 */
+	@CheckForNull
+	private String notAUtilityClass;
+
+	/**
+	 * Diese Methode startet und betreibt die Anwendung.
+	 *
+	 * @param args die zu verwendenden Argumente
+	 */
+	public static void main(@Nonnull final String[] args) {
 		SpringApplication.run(PuzzlesWebappApplication.class, args);
 	}
-
 }
---
+++ src/main/java/software/sirsch/sa4e/puzzlesWebapp/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Dieses Paket enthält eine Web-UI für das Puzzle-Protokoll über MQTT.
+ */
+package software.sirsch.sa4e.puzzlesWebapp;
